56819badc8ac4b01bbf38d8d4a17d036ebe57e13
faizan nerdware <mirzafaizanejaz@gmail.com>
2018-03-21 16:34:37 +0500

    Added some libraries

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 var express = require('express');
+var app = express();
+
+var path     = require('path');
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
