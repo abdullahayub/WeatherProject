b4a58599fb3960991690f981b05ce272a93b758a
Faizan Ubuntu <mirzafaizanejaz@gmail.com>
2018-03-21 17:10:38 +0500

    Removed Routes

--- src/routes/publicRouter.js
+++ src/routes/publicRouter.js
@@ -2,20 +2,4 @@ module.exports = function (app){
     app.get('/',function(req,res){
         res.render('index.ejs');
     });
-    app.get('/checkout',function(req,res){
-        res.render('checkout.ejs');
-    });
-    app.get('/logout', function (req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-    app.get('/about-us', function (req, res) {
-        res.render('about-us.ejs');
-    });
-    app.get('/we', function (req, res) {
-        res.render('we.ejs');
-    });
-    app.get('/test', function (req, res) {
-        res.render('font.ejs');
-    });
 };
